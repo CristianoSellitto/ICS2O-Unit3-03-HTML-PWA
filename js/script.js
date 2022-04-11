a1cbf86691c21ad2d6e1930e0706e18313eb2a24
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-11 16:37:41 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit3-03-HTML-PWA
+// April 11 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit3-03-HTML-PWA/sw.js", {
+    scope: "/ICS2O-Unit3-03-HTML-PWA/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Calculates the volume of a sphere using the radius the user provides in number fields.
  */
-function onButtonClick() {
-  alert("Hello, World!")
+function calculate() {
+  const radius = parseFloat(document.getElementById("radius").value)
+  const answer = ((4 / 3) * Math.PI * (radius ** 3)).toFixed(2)
+
+  document.getElementById("output").innerHTML = "<br><p>The volume of this sphere is " + answer + " cm³</p>"
 }
